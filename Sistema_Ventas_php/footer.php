6ca8873778c4210d1434d4487b8b19bbdcf64920
Fredd <150558494+5000fredd@users.noreply.github.com>
2023-11-12 16:52:59 -0400

    Delete Sistema_Ventas_php directory

--- Sistema_Ventas_php/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-	
-	<strong>Copyright &copy; 2023 <a href="https://www.Mbitious.com" target="_blank">Desarrollado por Millionaire System</a>.</strong>
-
-	Todos los derechos reservados.
-
-</footer>
