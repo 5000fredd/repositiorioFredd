3fbfa7405a6cfd5e1596f62be55571352f840e3c
Fredd <150558494+5000fredd@users.noreply.github.com>
2023-11-12 16:52:45 -0400

    Delete Sistema_Almacen_Distribucion_Trigo directory

--- Sistema_Almacen_Distribucion_Trigo/modulos/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-	
-	<strong>Copyright &copy; 2023 <a href="https://mendozakimetsu@gmail.com" target="_blank">Freddy Mendoza</a>.</strong>
-
-	Todos los derechos reservados.
-
-
-</footer>
